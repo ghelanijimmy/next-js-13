7612142be3f47b9b09cd53129abcc39b9496d679
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-12 12:30:06 +0000

    add header with icons and styling
    Merge-request: MAIN-MR-7
    Merged-by: Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>

--- imdb-clone/src/app/layout.tsx
+++ imdb-clone/src/app/layout.tsx
@@ -1,32 +1,41 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 import React from "react";
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'IMDB Clone',
-  description: 'This is the IMDB Clone',
-  viewport: 'width=device-width, initial-scale=1.0',
+  title: "IMDB Clone",
+  description: "This is the IMDB Clone",
+  viewport: "width=device-width, initial-scale=1.0",
   icons: [
     {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon.ico',
-        url: '/favicon.ico',
-    }
-  ]
-}
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon.ico",
+      url: "/favicon.ico",
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/*HEADER*/}
+        <Header />
+        {/*NAVBAR*/}
+
+        {/*SEARCH*/}
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ imdb-clone/src/components/MenuItem.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import { IconType } from "react-icons";
+import Link from "next/link";
+type MenuItemProps = {
+  title: string;
+  address: string;
+  Icon: IconType;
+};
+const MenuItem: FC<MenuItemProps> = ({ title, address, Icon }) => {
+  return (
+    <div>
+      <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
+        <Icon className="text-2xl sm:hidden mx-4" />
+        <p className="hidden sm:inline my-w text-sm">{title}</p>
+      </Link>
+    </div>
+  );
+};
+
+export default MenuItem;
