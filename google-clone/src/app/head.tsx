14d994d3d281d037571a06b281beb6ff13e6aabd
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-15 08:57:39 -0400

    initialize google clone project

---
+++ google-clone/src/app/head.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <>
+      <title>Google Clone Next.js 13</title>
+      <meta name="description" content="Next.js + TypeScript example" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+};
+
+export default Head;
